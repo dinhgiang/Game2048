68b48b112af1653e321d749f9d2766aef8ed02d7
dinhgiang <dinhvangiang1198@gmail.com>
2017-04-28 09:28:17 +0700

    Game2048_dinhgiang
    Project done

--- 2048_giang/main.cpp
+++ 2048_giang/main.cpp
@@ -4,7 +4,7 @@
 //
 //  Created by MacOS on 3/24/17.
 //  Copyright © 2017 MacOS. All rights reserved.
-//  
+//
 
 #include <iostream>
 #include <cstdlib>
@@ -19,17 +19,12 @@
 #include "logic.hpp"
 using namespace std;
 
-void createMenuGame() {
-    
-    
-}
-
 bool tryAgain(SDL_Renderer* &renderer, TTF_Font* &fontContinue, SDL_Event& event, SDL_Window* &window, SDL_Texture* &screenTexture) {
-    
+
     displayContinue(renderer, fontContinue);
     SDL_RenderPresent(renderer);
-    
-    do { 
+
+    do {
         SDL_Delay(10);
         if (SDL_WaitEvent(&event) == 0) { continue; }
         switch (event.key.keysym.sym) {
@@ -43,7 +38,7 @@ bool tryAgain(SDL_Renderer* &renderer, TTF_Font* &fontContinue, SDL_Event& event
                 break;
         }
     } while (event.key.keysym.sym != SDLK_y && event.key.keysym.sym != SDLK_n && event.type != SDL_QUIT);
-    
+
     return false;
 }
 bool playAGame(SDL_Window* &window, SDL_Renderer* &renderer, numTexture &numbersTexture, SDL_Event &event, SDL_Texture* &screenTexture, TTF_Font* &fontScore) {
@@ -51,19 +46,19 @@ bool playAGame(SDL_Window* &window, SDL_Renderer* &renderer, numTexture &numbers
     int score = 0;
     int array[4][4];
     int countEdit = 0;
-    
+
     for (int i = 0; i < 4; i++) {
         for (int j = 0; j < 4; j++) {
             array[i][j] = 0;
         }
     }
-    
+
     createRandNum(array);
     createRandNum(array);
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
     renderArray(array, window, numbersTexture, screenTexture, renderer);
     SDL_RenderPresent(renderer);
-    
+
     while (gameNotOver(array)) {
         SDL_Delay(10);
         if ( SDL_WaitEvent(&event) == 0) continue;
@@ -101,13 +96,13 @@ bool playAGame(SDL_Window* &window, SDL_Renderer* &renderer, numTexture &numbers
     renderLoseGame(array, numbersTexture, screenTexture, window, renderer);
     SDL_RenderPresent(renderer);
     SDL_Delay(1000);
-    
+
     return false;
 }
 
-int main(int argc, const char * argv[]) {
+int main(int argv, char** args) {
     srand((unsigned int) time(NULL));
-    
+
     SDL_Window *window = NULL;
     SDL_Renderer *renderer = NULL;
     SDL_Texture *screenTexture = NULL;
@@ -115,14 +110,14 @@ int main(int argc, const char * argv[]) {
     SDL_Event event;
     TTF_Font *fontScore = NULL;
     TTF_Font *fontContinue = NULL;
-    
+
     initSDL(window, renderer, fontScore, fontContinue, numbersTexture);
-    
-    
+
+
     do {
         if ( playAGame(window, renderer, numbersTexture, event, screenTexture, fontScore) ) break;
     } while ( tryAgain(renderer, fontContinue, event, window, screenTexture) );
-    
+
     quitSDL(window, renderer, screenTexture);
     return 0;
 }
